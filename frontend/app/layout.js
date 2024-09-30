298852c24bf5a45edd92a86fc4a89670f4a7a7b2
Enzo Hugonnier <enzohugonn@gmail.com>
2024-09-30 20:09:47 +0200

    refactor(Frontend Files): Move fonts into assets folder

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,12 +2,12 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./Assets/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./Assets/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
