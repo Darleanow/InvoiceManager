182053a1fe6beeef88a02edb4965d0143862bdf8
Jaden38 <damien.nithard@gmail.com>
2024-10-02 17:55:13 +0200

    feat(Routes): Add traffic limiter to routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 const examplesController = require('../controllers/examples/examples');
 const invoicesController = require('../controllers/invoices/invoices');
 const benefitsController = require('../controllers/benefits/benefits');
 const customersController = require('../controllers/customers/customers');
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 2000, 
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+router.use(limiter);
 
 router.post('/invoices', invoicesController.createInvoice);
 router.get('/invoices', invoicesController.getInvoices);
