e0d4864b63a7493dfae6b433579bc73d17b91f2c
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-02 14:50:24 +0200

    feat(Navbar): Add icons & button to navbar

--- frontend/app/Assets/components/Navbar/navbar.jsx
+++ frontend/app/Assets/components/Navbar/navbar.jsx
@@ -1,9 +1,28 @@
-function Navbar() {
-    return () => {
-        return (
-            <div>
-                <h1>Navbar</h1>
-            </div>
-        )
-    }
+import React from 'react';
+import styles from './style.css';
+import { AiFillDollarCircle } from "react-icons/ai";
+import { IoIosPeople } from "react-icons/io";
+import { BiSolidReport } from "react-icons/bi";
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <div>
+                        <AiFillDollarCircle />
+                        <li>Billing</li>
+                    </div>
+                    <div>
+                        <IoIosPeople />
+                        <li>Clients</li>
+                    </div>
+                    <div>
+                        <BiSolidReport />
+                        <li>Reporting</li>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    )
 }
