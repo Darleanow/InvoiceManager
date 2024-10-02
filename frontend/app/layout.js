71fdafa4b2139b1614fe9fd6da1908ae7c1413bf
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-02 12:56:52 +0200

    fix(Project): Clean up project

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,17 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./Assets/fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./Assets/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata = {
   title: "Invoice Manager",
   description: "A platform to manage your invoices",
@@ -20,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
