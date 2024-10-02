63419e88b75137c47975b73362ba73e15166e438
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-02 17:10:46 +0200

    feat(SubNavigation): Add placeholder for complex components & setup layout logic

---
+++ frontend/app/Assets/components/SubNavigationBilling/SubNavigationBilling.jsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./SubNavigationBilling.module.css";
+
+export default function SubNavigationBilling() {
+  const [currentSubNav, setCurrentSubNav] = useState("overview");
+  const [range, setRange] = useState("month"); // Dynamically control range
+
+  // Function to handle subnav change
+  const handleSubNavChange = (subNav) => {
+    setCurrentSubNav(subNav);
+  };
+
+  return (
+    <>
+      <h1 className={styles.billing_title}>Billing</h1>
+      <div className={styles.billing_subnav}>
+        <div className={styles.billing_subnav_items}>
+          <p
+            className={
+              currentSubNav === "overview"
+                ? `${styles.billing_subnav_item_active}`
+                : ""
+            }
+            onClick={() => handleSubNavChange("overview")}
+          >
+            Overview
+          </p>
+          <p
+            className={
+              currentSubNav === "quotation"
+                ? `${styles.billing_subnav_item_active}`
+                : ""
+            }
+            onClick={() => handleSubNavChange("quotation")}
+          >
+            Quotation
+          </p>
+          <p
+            className={
+              currentSubNav === "invoice"
+                ? `${styles.billing_subnav_item_active}`
+                : ""
+            }
+            onClick={() => handleSubNavChange("invoice")}
+          >
+            Invoice
+          </p>
+        </div>
+        <hr className={styles.separator_line} />
+        {/* TODO: Add filters here */}
+        <div className={styles.filter_options}>
+          <div className={styles.date_picker_place_holder_left} />
+          <div className={styles.date_picker_place_holder_right} />
+          <p className={styles.compared_to_text}>compared to</p>
+          <div className={styles.compared_to_dropdown_period}></div>
+          <div
+            className={styles.export_button}
+            onClick={() => {
+              alert("T'exporte rien");
+            }}
+          >
+            Export
+          </div>
+        </div>
+      </div>
+      <div className={styles.billing_subnav_content}>
+        <div className={styles.billing_monthly_revenue}></div>
+        <div className={styles.outstanding_invoices}></div>
+        <div className={styles.empty_content}></div>
+        <div className={styles.empty_content}></div>
+      </div>
+    </>
+  );
+}
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Assets/components/Navbar/navbar";
 import Topbar from "./Assets/components/Topbar/Topbar";
+import SubNavigationBilling from "./Assets/components/SubNavigationBilling/SubNavigationBilling";
 
 export default function Home() {
   return (
     <div>
       <Topbar />
       <Navbar />
+      <SubNavigationBilling />
     </div>
   );
 }
