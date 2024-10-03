316c6f7f88f3996977c05686da0dabe45f8b4823
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-03 12:21:22 +0200

    style(Topbar): Add new style to topbar

--- frontend/app/Assets/components/Topbar/Topbar.jsx
+++ frontend/app/Assets/components/Topbar/Topbar.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 import styles from "./styles.module.css";
-
-import { IoIosNotifications } from "react-icons/io";
-import { IoIosSettings } from "react-icons/io";
-import { IoIosSearch } from "react-icons/io";
-
+import { IoIosNotifications, IoIosSettings, IoIosSearch, IoIosArrowDown } from "react-icons/io";
 
 export default function Topbar() {
-    
-  
     return (
-        <div classname={styles.topbar_container}>
+        <div className={styles.topbar_container}>
             <div className={styles.topbar_left_pannel}>
                 <p className={styles.topbar_text}>InMa</p>
+                <div className={styles.topbar_vertical_line}></div>
                 <p className={styles.topbar_user}>Username</p>
+                <IoIosArrowDown className={styles.topbar_arrow} />
             </div>
             <div className={styles.topbar_middle_pannel}>
                 <div className={styles.topbar_search_container}>
-                    <IoIosSearch classname={styles.topbar_search_icon}/>
+                    <IoIosSearch className={styles.topbar_search_icon} />
                     <input type="text" placeholder="Search" className={styles.topbar_search} />
                 </div>
             </div>
             <div className={styles.topbar_right_pannel}>
-                <IoIosNotifications className={styles.topbar_notification}/>
+                <IoIosNotifications className={styles.topbar_notification} />
                 <IoIosSettings className={styles.topbar_settings} />
-                <button>Create Invoice</button>
+                <div className={styles.topbar_vertical_line}></div>
+                <button className={styles.topbar_button}>Create Invoice</button>
             </div>
         </div>
     );
