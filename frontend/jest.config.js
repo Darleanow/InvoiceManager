260571e6aff27f1a57e6ac222f6033dc24e9c7c8
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-03 21:38:09 +0200

    fix(JestCoverage): Try reproducing the issue with a minimalistic example

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,19 +1,8 @@
 module.exports = {
-  testPathIgnorePatterns: ["/node_modules/", "/end_to_end/"],
+  //   testPathIgnorePatterns: ["/node_modules/", "/end_to_end/"],
   collectCoverage: true,
   coverageReporters: ["lcov", "text"],
-  collectCoverageFrom: [
-    "app/Assets/components/**/*.{js,jsx,ts,tsx}", // specific needed target
-    "**/*.{js,jsx,ts,tsx}", // general pattern
-    "!**/node_modules/**",
-    "!**/end_to_end/**",
-    "!**/step_definitions/**",
-    "!**/coverage/**",
-    "!**/dist/**",
-    "!**/*.config.js",
-    "!**/.*",
-    "!cucumber.js", // Config file, unrelevant for the coverage
-  ],
+  collectCoverageFrom: ["./app/**"],
   coverageThreshold: {
     global: {
       branches: 0,
