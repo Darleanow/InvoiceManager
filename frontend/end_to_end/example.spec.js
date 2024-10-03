842a683e7c1e8a1c1952e670364df588e823fb07
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-03 16:09:15 +0200

    feat(PlayWright): Setup playwright library and add some examples

---
+++ frontend/end_to_end/example.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("homepage has title and links to other page", async ({ page }) => {
+  await page.goto("https://google.com");
+
+  await expect(page).toHaveTitle(/Google/);
+});
