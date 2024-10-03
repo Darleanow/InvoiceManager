256bc4c80007c3d32857668820beca608e90afc5
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-03 22:46:44 +0200

    fix(PlayWright): Remove timeout in test & set back to default the original timeout length

--- frontend/end_to_end/example.spec.js
+++ frontend/end_to_end/example.spec.js
@@ -1,11 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test(
-  "homepage has title and links to other page",
-  async ({ page }) => {
-    await page.goto("https://google.com");
+test("homepage has title and links to other page", async ({ page }) => {
+  await page.goto("https://google.com");
 
-    await expect(page).toHaveTitle(/Google/);
-  },
-  { timeout: 10000 }
-);
+  await expect(page).toHaveTitle(/Google/);
+});
