68dfd09bf6b11467b85ea4288e6dd466a7510a16
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-04 16:23:40 +0200

    fix(unit-test): Fix UT

--- frontend/app/Assets/components/Navbar/Navbar.test.js
+++ frontend/app/Assets/components/Navbar/Navbar.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar.jsx";
 import "@testing-library/jest-dom";
 import punycode from 'punycode';
 
