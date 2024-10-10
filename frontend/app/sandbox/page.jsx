224bdb25ab6e19f3e7d8b1b13b1ec6289d596c30
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-10 18:04:27 +0200

    feat(Atoms): Add SeparatorLine component

--- frontend/app/sandbox/page.jsx
+++ frontend/app/sandbox/page.jsx
@@ -1,5 +1,10 @@
 import Logo from "../components/atoms/Logo";
+import SeparatorLine from "../components/atoms/SeparatorLine";
+import styles from "./page.module.scss";
 
 export default function SandBox() {
-  return <Logo />;
+  return <div className={styles.main}>
+    <Logo />
+    <SeparatorLine />
+  </div>;
 }
