615cfc14278117029eaa45c535a50496e49cb6d3
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-10 16:47:37 +0200

    fix(CI): Remove E2E tests as they are not relevant anymore

--- frontend/end_to_end/example.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("homepage has title and links to other page", async ({ page }) => {
-  await page.goto("https://google.com");
-
-  await expect(page).toHaveTitle(/Google/);
-});
