dd0a090fa7cc1e5d7207851cc43c522a77b3e6c7
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-14 21:25:18 +0200

    feat(Navbar): Implement navbar

--- frontend/app/components/_templates/HomePage/HomePage.jsx
+++ frontend/app/components/_templates/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import styles from './HomePage.module.scss';
 
 import TopBar from '../../_organisms/TopBar/TopBar';
+import NavBar from '../../_organisms/NavBar/NavBar';
 
 export default function HomePage() {
   return (
     <div className={styles.home_page}>
       <TopBar />
+      <NavBar />
     </div>
   );
 }
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
