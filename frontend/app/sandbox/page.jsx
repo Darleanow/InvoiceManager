8b53da737d79f4d67c3ed4fb4397c5aa3864b9c6
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-15 18:46:56 +0200

    feat(Atoms/Organisms): Add create invoice topbar

--- frontend/app/sandbox/page.jsx
+++ frontend/app/sandbox/page.jsx
@@ -1,12 +1,12 @@
 'use client';
 
-import HomePage from '../components/_templates/HomePage/HomePage';
 import styles from './page.module.scss';
+import CreateInvoiceBar from '../components/_organisms/CreateInvoiceBar/CreateInvoiceBar';
 
 export default function SandBox() {
   return (
     <div className={styles.main}>
-      <HomePage />
+      <CreateInvoiceBar />
     </div>
   );
 }
