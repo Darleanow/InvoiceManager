e892e9f495b37247bc1b2e66f77057e88e981ed8
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-15 18:46:56 +0200

    feat(Atoms/Organisms): Add create invoice topbar

---
+++ frontend/app/components/_organisms/CreateInvoiceBar/CreateInvoiceBar.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import Logo from '../../_atoms/Logo/Logo';
+import SeparatorLine from '../../_atoms/SeparatorLine/SeparatorLine';
+import ImportExistingInvoices from '../../_atoms/ImportExistingInvoices/ImportExistingInvoices';
+import styles from './CreateInvoiceBar.module.scss';
+import { AiOutlineClose } from 'react-icons/ai';
+
+export default function CreateInvoiceBar() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Use requestAnimationFrame for smoother appearance animation
+    const timeoutId = setTimeout(() => setIsVisible(true), 100);
+
+    return () => clearTimeout(timeoutId); // Cleanup timeout
+  }, []);
+
+  return (
+    <div
+      className={`${styles.create_invoice_bar_container} ${isVisible ? styles.appear : ''}`}
+    >
+      <div className={styles.left_container}>
+        <Logo />
+        <SeparatorLine />
+        <p className={styles.create_invoice_text}>Create Invoice</p>
+        <SeparatorLine />
+        <ImportExistingInvoices />
+      </div>
+      <div className={styles.right_container}>
+        <AiOutlineClose className={styles.close_icon} />
+      </div>
+    </div>
+  );
+}
