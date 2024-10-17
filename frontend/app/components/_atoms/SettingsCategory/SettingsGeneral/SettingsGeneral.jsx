73f570479e81f0e436a53e68d7b4637adaf4e08a
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-17 23:03:12 +0200

    style(Settings): Add new styles to settings

--- frontend/app/components/_atoms/SettingsCategory/SettingsGeneral/SettingsGeneral.jsx
+++ frontend/app/components/_atoms/SettingsCategory/SettingsGeneral/SettingsGeneral.jsx
@@ -14,22 +14,22 @@ import styles from './SettingsGeneral.module.scss';
 export default function SettingsGeneral() {
   return (
     <div>
-      <h1>SettingsGeneral</h1>
+      <h1 className={styles.title}>SettingsGeneral</h1>
       <div className={styles.general_container}>
-        <h1> Company Details</h1>
+        <h3> Company Details</h3>
         <CompanyName />
         <CompanyLogo />
         <ContactInformation />
         <Adress />
       </div>
       <div className={styles.localization_container}>
-        <h1>Localization</h1>
+        <h3>Localization</h3>
         <DateFormat/>
         <TimeZone/>
         <Language/>
       </div>
-      <div className='currency_container'>
-        <h1>Currency</h1>
+      <div className={styles.currency_container}>
+        <h3>Currency</h3>
         <DefaultCurrency/>
         <CurrencySymbolPosition/>
       </div>
