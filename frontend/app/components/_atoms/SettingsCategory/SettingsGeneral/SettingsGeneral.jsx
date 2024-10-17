db53986922239d9cd20191d8b6c8ccbf077c7aa8
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-17 14:40:12 +0200

    feat(Settings): Add new repository into code

--- frontend/app/components/_atoms/SettingsCategory/SettingsGeneral/SettingsGeneral.jsx
+++ frontend/app/components/_atoms/SettingsCategory/SettingsGeneral/SettingsGeneral.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import CompanyName from './CompanyName/CompanyName';
-import CompanyLogo from './CompanyLogo/CompanyLogo';
-import ContactInformation from './ContactInformation/ContactInformation';
-import Adress from './Adress/Adress';
+import CompanyName from './CompanyDetails/CompanyName/CompanyName';
+import CompanyLogo from './CompanyDetails/CompanyLogo/CompanyLogo';
+import ContactInformation from './CompanyDetails/ContactInformation/ContactInformation';
+import Adress from './CompanyDetails/Adress/Adress';
 
 export default function SettingsGeneral() {
   return (
     <div>
       <h1>SettingsGeneral</h1>
-      <CompanyName />
-      <CompanyLogo />
-      <ContactInformation />
-      <Adress />
+      <div className={styles.general_container}>
+        <h1> Company Details</h1>
+        <CompanyName />
+        <CompanyLogo />
+        <ContactInformation />
+        <Adress />
+      </div>
     </div>
   );
 }
