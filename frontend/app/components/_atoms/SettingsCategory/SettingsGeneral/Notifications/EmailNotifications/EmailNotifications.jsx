b4903a0da4f6630c74d79877bd1de9dfcc56983a
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-17 23:44:35 +0200

    style(Settings): Add new styles

--- frontend/app/components/_atoms/SettingsCategory/SettingsGeneral/Notifications/EmailNotifications/EmailNotifications.jsx
+++ frontend/app/components/_atoms/SettingsCategory/SettingsGeneral/Notifications/EmailNotifications/EmailNotifications.jsx
@@ -1,48 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './EmailNotifications.module.scss';
 
 const EmailNotifications = () => {
-  const [selectedOption, setSelectedOption] = useState('none');
-
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
-
   return (
-    <div className={styles.notificationsContainer}>
-      <div className={styles.checkboxLabel}>
-        <input 
-          type="radio" 
-          id="none" 
-          name="emailNotifications" 
-          value="none" 
-          checked={selectedOption === 'none'} 
-          onChange={handleOptionChange} 
-        />
-        <label htmlFor="none">No alerts</label>
-      </div>
-      <div className={styles.checkboxLabel}>
-        <input 
-          type="radio" 
-          id="overdue" 
-          name="emailNotifications" 
-          value="overdue" 
-          checked={selectedOption === 'overdue'} 
-          onChange={handleOptionChange} 
-        />
-        <label htmlFor="overdue">Alert when an invoice is overdue</label>
-      </div>
-      <div className={styles.checkboxLabel}>
-        <input 
-          type="radio" 
-          id="paid" 
-          name="emailNotifications" 
-          value="paid" 
-          checked={selectedOption === 'paid'} 
-          onChange={handleOptionChange} 
-        />
-        <label htmlFor="paid">Alert when an invoice is paid</label>
-      </div>
+    <div className={styles.container}>
+      <label className={styles.label}>
+        <input type="radio" name="emailNotification" value="overdue" className={styles.radio} />
+        When an invoice is overdue
+      </label>
+      <label className={styles.label}>
+        <input type="radio" name="emailNotification" value="paid" className={styles.radio} />
+        When an invoice is paid
+      </label>
     </div>
   );
 };
