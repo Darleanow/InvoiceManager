b61eddd92534965990342ccf447317a2096ef3e3
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-19 10:40:34 +0200

    feat(CreateInvoice): Add Product entry line (for the dropdown menu

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
