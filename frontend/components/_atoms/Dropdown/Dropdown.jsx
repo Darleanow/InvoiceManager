6bf38e023a55c8e6c45f1212df9b209ea6f5b643
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-22 11:56:48 +0200

    fix(Styles & SonarLint): Fix styles and sonarlint issues

--- frontend/components/_atoms/Dropdown/Dropdown.jsx
+++ frontend/components/_atoms/Dropdown/Dropdown.jsx
@@ -1,40 +1,45 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import styles from './Dropdown.module.scss';
 import { FaChevronDown } from 'react-icons/fa';
 
 export default function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleSelect = (selectedValue) => {
-    onChange(selectedValue);
-    setIsOpen(false);
-  };
+  const toggleDropdown = () => setIsOpen((prev) => !prev);
 
   return (
-    <div className={styles.dropdown_custom}>
-      <div className={styles.dropdown_header} onClick={toggleDropdown}>
-        {value}{' '}
+    <div
+      className={`${styles.dropdown_custom} ${isOpen ? styles.open : styles.closed}`}
+    >
+      <button className={styles.dropdown_header} onClick={toggleDropdown}>
+        {value}
         <span className={isOpen ? styles.arrow_up : styles.arrow_down}>
           <FaChevronDown className={styles.icon_arrow} />
         </span>
-      </div>
+      </button>
       {isOpen && (
         <div className={styles.dropdown_list}>
           {options.map((option) => (
-            <div
+            <button
               key={option}
               className={styles.dropdown_item}
-              onClick={() => handleSelect(option)}
+              onClick={() => {
+                onChange(option);
+                setIsOpen(false);
+              }}
             >
               {option}
-            </div>
+            </button>
           ))}
         </div>
       )}
     </div>
   );
 }
+
+Dropdown.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
