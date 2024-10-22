0d8a71beca9ffacfac7aa01d0e5ced0d753ac08c
Enzo Hugonnier <enzohugonn@gmail.com>
2024-10-22 11:56:48 +0200

    fix(Styles & SonarLint): Fix styles and sonarlint issues

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,10 +1,4 @@
-import {
-  ClerkProvider,
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
+import { ClerkProvider, RedirectToSignIn, SignedOut } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 import PropTypes from 'prop-types';
 import '../styles/globals.scss';
