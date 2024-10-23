e8a887ede1c50c424de0eacc16d01912cb49af3e
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-23 23:36:38 +0200

    feat(Settings): Add new files && styles

--- frontend/app/components/_atoms/SettingsCategory/SettingsClients/SettingsClients.jsx
+++ frontend/app/components/_atoms/SettingsCategory/SettingsClients/SettingsClients.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
+import styles from './SettingsClients.module.scss';
+
+
 export default function SettingsClients() {
   return (
-    <div>
-      <h1>SettingsClients</h1>
+    <div className={styles.page}>
+      <h1 className={styles.title}>
+        SettingsClients
+        </h1>
+        <div>
+          
+        </div>
     </div>
   );
 }
